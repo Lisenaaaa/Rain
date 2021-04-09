46c34da7d82268cfef1b56ed75e0b70dc6a17053
Zordlan <fiercefalcon14@gmail.com>
2021-04-09 14:32:32 -0700

    are messages really that important

--- src/commands/testing/newcommand.ts
+++ src/commands/testing/newcommand.ts
@@ -1,4 +1,5 @@
 import { Command } from "discord-akairo";
+import { TextChannel } from "discord.js";
 
 export default class newcommand extends Command {
     constructor() {
@@ -8,6 +9,13 @@ export default class newcommand extends Command {
     }
 
     async exec(message) {
-        await message.channel.send(`pain`)
+        if (message.author.id = "492488074442309642") {
+            await message.channel.send(`pain`)
+            const channel = this.client.channels.cache.get('824680358494076959') as TextChannel
+            channel.send("hello")
+        }
+        else {
+            message.channel.send("This command is used for testing purposes, and can only be used by Zordlan.")
+        }
     }
 }
