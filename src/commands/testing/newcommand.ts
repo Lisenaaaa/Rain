dea2e0bcf29a39448394ec6f0afbf3422ac6a6c7
Zordlan <fiercefalcon14@gmail.com>
2021-04-09 16:40:23 -0700

    pi test

--- src/commands/testing/newcommand.ts
+++ src/commands/testing/newcommand.ts
@@ -10,9 +10,7 @@ export default class newcommand extends Command {
 
     async exec(message) {
         if (message.author.id = "492488074442309642") {
-            await message.channel.send(`pain`)
-            const channel = this.client.channels.cache.get('824680358494076959') as TextChannel
-            channel.send("hello")
+            await message.channel.send(`HI THIS IS ON A RASPBERRY PI`)
         }
         else {
             message.channel.send("This command is used for testing purposes, and can only be used by Zordlan.")
