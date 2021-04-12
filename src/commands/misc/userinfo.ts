bf1ba0105b914180231593fb56545aef739fb674
Zordlan <fiercefalcon14@gmail.com>
2021-04-12 13:59:53 -0700

    pain is fun

--- src/commands/misc/userinfo.ts
+++ src/commands/misc/userinfo.ts
@@ -4,16 +4,21 @@ import { MessageEmbed } from "discord.js";
 export default class userinfo extends Command {
 	constructor() {
 		super("userinfo", {
-			aliases: ["userinfo", "uinfo", "ui"] 
+			aliases: ["userinfo", "uinfo", "ui"]
 		});
 	}
 
 	exec(message) {
-        const infoembed = new MessageEmbed()
-		.setTitle(message.author.tag)
-        .setThumbnail(message.author.displayAvatarURL)
+		const infoembed = new MessageEmbed()
+			.setTitle(message.author.tag)
+			.setThumbnail(`https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=2048`)
+			.setDescription(`
+			**Mention:** ${message.author}
+			**ID:** \`${message.author.id}\`
+			**Created at:** ${message.author.createdTimestamp}
+			`)
 
-        
-        message.channel.send(infoembed)
+
+		message.channel.send(infoembed)
 	}
 }
