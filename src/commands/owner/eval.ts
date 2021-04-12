8ddec9e55ab85ab9538de5a2ffebf32b589ead8f
Zordlan <fiercefalcon14@gmail.com>
2021-04-12 16:49:05 -0700

    i have almost gotten to the point where my eval command doesnt let me leak my token

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
 import { Command } from 'discord-akairo';
+import { TextChannel } from 'discord.js';
 import { MessageEmbed } from 'discord.js';
 import { promisify } from 'util';
 
@@ -29,25 +30,35 @@ export default class evaluate extends Command {
 
     async exec(message, args) {
 
-        if (args.codetoeval.includes("client.token")) {
-            return message.channel.send(`yeah no you dont get to leak my token`)
-        }
+        // if (args.codetoeval.includes("client.token")) {
+        //     return message.channel.send(`I see that you have tried to run code that (probably) gets my token! I don't like people gaining full control over me, so I won't be evaluating that code.`)
+        // }
+        // if (args.codetoeval.includes(".env")) {
+        //     return message.channel.send(`I see that you have tried to run code that (probably) gets my token! I don't like people gaining full control over me, so I won't be evaluating that code.`)
+        // }
 
-        if (args.silent) {
-            return eval(args.codetoeval)
-            //return message.channel.send('This eval command has the silent tag!')
-        }
-
-        //eval(args.codetoeval)
         const output = await eval(args.codetoeval)
-
+        console.log(output)
+        
         const newoutput = output.stdout
+        const tokencheck = output.content
+
+        if(tokencheck?.includes(process.env["token"])) {
+            message.channel.send(`<@492488074442309642> somebody, possibly you, has leaked my token!`)
+            const errorchannel = this.client.channels.cache.get('824680761470746646') as TextChannel
+            errorchannel.send('<@492488074442309642> TOKEN HAS BEEN LEAKED')
+            errorchannel.send('<@492488074442309642> TOKEN HAS BEEN LEAKED')
+            errorchannel.send('<@492488074442309642> TOKEN HAS BEEN LEAKED')
+            errorchannel.send('<@492488074442309642> TOKEN HAS BEEN LEAKED')
+            errorchannel.send('<@492488074442309642> TOKEN HAS BEEN LEAKED')
+
+        }
 
         //console.log(`---------`)
         const evaloutputembed = new MessageEmbed()
         .setTitle('Evaluated Code')
         .setDescription(`
-        :inbox_tray: **Code**
+        :inbox_tray: **Input**
         \`\`\`${args.codetoeval}\`\`\`
         
         :outbox_tray: **Output**
