e99b554630ef4ca4ca0807569ef4c133fcf4bae1
Zordlan <fiercefalcon14@gmail.com>
2021-04-26 17:58:40 -0700

    this is a commit

---
+++ src/commands/owner/gitpull.ts
@@ -0,0 +1,26 @@
+import { exec } from 'child_process';
+import { Command } from 'discord-akairo';
+import { MessageEmbed } from 'discord.js';
+import { promisify } from 'util';
+
+const sh = promisify(exec);
+
+export default class gitpull extends Command {
+    constructor() {
+        super('gitpull', {
+            aliases: ['gitpull', 'pull'],
+            ownerOnly: true,
+            channel: 'guild'
+        });
+    }
+
+    async exec(message, args) {
+        const githubembed = new MessageEmbed()
+        
+        let pull = await eval(`sh('git pull')`)
+        githubembed.setDescription(`\`\`\`js\n${gitpull}\`\`\``)
+
+        message.channel.send(githubembed)
+
+    }
+}
