cf23aea446eff08bd515d458fb30d5b6bea66028
Zordlan <fiercefalcon14@gmail.com>
2021-04-27 08:25:08 -0700

    automod example

---
+++ src/listeners/automod.ts
@@ -0,0 +1,20 @@
+import { Listener } from 'discord-akairo';
+import moderation from '../functions/moderation';
+
+class automodListener extends Listener {
+    constructor() {
+        super('automod', {
+            emitter: 'client',
+            event: 'message'
+        });
+    }
+
+    exec(message) {
+        if (message.content.includes(`eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee`)) {
+            message.delete()
+            moderation.ban(message.member, `Automod | Banned Words`, message.author, message)
+        }
+    }
+}
+
+module.exports = automodListener;
