c4f397177be6a0d5eb3836f4f96cc6d51ff94e28
Zordlan <fiercefalcon14@gmail.com>
2021-05-04 08:35:43 -0700

    ok so apparently its username and not name

--- src/listeners/serverSpecific/fuckyoubot.ts
+++ src/listeners/serverSpecific/fuckyoubot.ts
@@ -11,7 +11,7 @@ class fuckyoubot extends Listener {
     async exec(message) {
         //fuck you bot
         if (message.content.toLowerCase().includes(`fuck`) && message.author.bot == false && message.guild.id == `794610828317032458`) {
-            message.channel.send(`fuck you, ${message.author.name}`)
+            message.channel.send(`fuck you, ${message.author.username}`)
         }
 
         //you can probably tell what this does
