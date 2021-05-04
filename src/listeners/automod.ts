d3ed7ee192ec60c88000c4b277454100c987cf45
Zordlan <69115669+Zordlan@users.noreply.github.com>
2021-05-03 21:25:33 -0700

    oh my fucking god i hate my parents
    internet turns off at 9, and i figured out how to do what i wanted to do at 8:55, so uh yeah
    
    also fuck not having a database and using actual files that i have gitignored

--- src/listeners/automod.ts
+++ src/listeners/automod.ts
@@ -1,33 +1,33 @@
-import { Listener } from 'discord-akairo';
-import moderation from '../functions/moderation';
-import utils from '../functions/utils';
+// import { Listener } from 'discord-akairo';
+// import moderation from '../functions/moderation';
+// import utils from '../functions/utils';
 
-const jsonfile = require('jsonfile')
-const file = 'config/global/bannedwords.json'
+// const jsonfile = require('jsonfile')
+// const file = 'config/global/bannedwords.json'
 
-class automodListener extends Listener {
-    constructor() {
-        super('automod', {
-            emitter: 'client',
-            event: 'message'
-        });
-    }
+// class automodListener extends Listener {
+//     constructor() {
+//         super('automod', {
+//             emitter: 'client',
+//             event: 'message'
+//         });
+//     }
 
-    exec(message) {
-        let hasTriggered = false
+//     exec(message) {
+//         let hasTriggered = false
 
-        jsonfile.readFile(file)
-            .then(badwords => {
-                badwords.bannedwords.forEach(word => {
-                    if (message.content.includes(word) && hasTriggered != true) {
+//         jsonfile.readFile(file)
+//             .then(badwords => {
+//                 badwords.bannedwords.forEach(word => {
+//                     if (message.content.includes(word) && hasTriggered != true) {
 
-                        message.channel.send(`No!`)
-                        hasTriggered = true
-                    }
-                });
-            })
-            .catch(error => utils.errorhandling(error, message))
-    }
-}
+//                         message.channel.send(`No!`)
+//                         hasTriggered = true
+//                     }
+//                 });
+//             })
+//             .catch(error => utils.errorhandling(error, message))
+//     }
+// }
 
-module.exports = automodListener;
+// module.exports = automodListener;
