42125fd17befd757090ff9df8f4927e835964318
Zordlan <69115669+Zordlan@users.noreply.github.com>
2021-05-06 10:05:32 -0700

    fuck you vsc

--- src/commands/owner/reload.ts
+++ src/commands/owner/reload.ts
@@ -2,7 +2,7 @@ import { Command } from "discord-akairo";
 import { BotCommand } from "../../extensions/BotCommand"
 import utils from '../../functions/utils'
 
-export default class reload extends Command {
+export default class reload extends BotCommand {
     constructor() {
         super("reload", {
             aliases: ["reload"],
@@ -14,7 +14,7 @@ export default class reload extends Command {
     async exec(message) {
         //THIS IS BROKEN AND DOES LITERALLY NOTHING RIGHT NOW OTHER THAN SEND A MESSAGE AND I DONT KNOW WHY SOMEONE PLEASE HELP AAAAAAAAAAAAAAAAAAA
         try {
-            this.handler.reloadAll()
+            this.client.commandHandler.reloadAll()
             message.channel.send(`Maybe reloaded commands?`)
         }
         catch (err) {
