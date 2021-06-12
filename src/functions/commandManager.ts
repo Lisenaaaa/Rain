69067cead876f1a11c80a71d757c24b8abd3bfd8
Zordlan <fiercefalcon14@gmail.com>
2021-06-11 19:58:14 -0700

    fml

--- src/functions/commandManager.ts
+++ src/functions/commandManager.ts
@@ -1,23 +1,27 @@
+import chalk from "chalk"
 import { Message } from "discord.js"
 import database from "./database"
+import guildSettings from "./guildSettings"
+import utils from "./utils"
 
 async function checkIfCommandCanBeUsed(message: Message, commandID: string) {
     let commandCanBeRan = false
 
-    //let permissionsNeeded = await database.readCommand(commandID)[0]
+    const userID = message.author.id
+    const channelID = message.channel.id
+    const ownerID = message.guild.me.guild.me.guild.me.guild.me.guild.owner.id
+    const guildID = message.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.me.guild.id
 
-    //let userHasPerms = false
-    database.read(message.guild.id).then(async settings => {
-        database.readCommand(commandID).then(async botSettings => {
-            const guildSettings = settings[0].commandSettings[commandID]
-            const globalSettings = botSettings[0]
+    const userPerms = await guildSettings.getUserPerms(message)
 
-            let disabledChannels = guildSettings.disabledChannels
-            let allowedRoles = guildSettings.allowedRoles
+    const commandCanBeUsedInChannel = await guildSettings.checkUserCanUseCommandInChannel(guildID, channelID, userPerms, commandID)
 
-            
-        })
-    })
+    const checkCommandEnabled = await database.checkCommandEnabled(commandID)
+
+
+    console.log(chalk`{bgBlue ${message.author.username}'s permissions} ${userPerms}`)
+    console.log(chalk`{bgBlue Can the user run the command in this channel} ${commandCanBeUsedInChannel}`)
+    console.log(chalk`{bgRed Command status, overriding all other checks} ${checkCommandEnabled}`)
 }
 
 export = {
--- src/functions/guildSettings.ts
+++ src/functions/guildSettings.ts
@@ -14,7 +14,7 @@ const permNames = {
 async function getUserPerms(message: Message) {
     return database.read(message.guild.id).then(settings => {
         let found = false
-        let perms = ``
+        let perms
 
         const roleSettings = settings[0].guildSettings.staffRoles
 
@@ -28,31 +28,31 @@ async function getUserPerms(message: Message) {
         message.member.roles.cache.forEach(role => {
             if (role == owner && found == false) {
                 found = true
-                return perms = `owner`
+                return perms = 'owner'
             }
             else if (role == admin && found == false) {
                 found = true
-                return perms = `admin`
+                return perms = 'admin'
             }
             else if (role == srMod && found == false) {
                 found = true
-                return perms = `srMod`
+                return perms = 'srMod'
             }
             else if (role == moderator && found == false) {
                 found = true
-                return perms = `moderator`
+                return perms = 'moderator'
             }
             else if (role == helper && found == false) {
                 found = true
-                return perms = `helper`
+                return perms = 'helper'
             }
             else if (role == trialHelper && found == false) {
                 found = true
-                return perms = `trialHelper`
+                return perms = 'trialHelper'
             }
         })
 
-        if (message.author.id == message.guild.ownerID) { perms = `owner` }
+        //if (message.author.id == message.guild.ownerID) { perms = 'owner' }
         return perms
     })
 }
@@ -60,35 +60,40 @@ async function getUserPerms(message: Message) {
 function getAllUserPerms(userPerms:string) {
     let perms
 
-    if (userPerms == `everyone`) { perms = [`everyone`] }
-    if (userPerms == `trialHelper`) { perms = [`everyone`, `trialHelper`] }
-    if (userPerms == `helper`) { perms = [`everyone`, `trialHelper`, `helper`] }
-    if (userPerms == `moderator`) { perms = [`everyone`, `trialHelper`, `helper`, `moderator`] }
-    if (userPerms == `srMod`) { perms = [`everyone`, `trialHelper`, `helper`, `moderator`, `srMod`] }
-    if (userPerms == `admin`) { perms = [`everyone`, `trialHelper`, `helper`, `moderator`, `srMod`, `admin`] }
-    if (userPerms == `owner`) { perms = [`everyone`, `trialHelper`, `helper`, `moderator`, `srMod`, `admin`, `owner`] }
+    if (userPerms == 'everyone') { perms = ['everyone'] }
+    if (userPerms == 'trialHelper') { perms = ['everyone', 'trialHelper'] }
+    if (userPerms == 'helper') { perms = ['everyone', 'trialHelper', 'helper'] }
+    if (userPerms == 'moderator') { perms = ['everyone', 'trialHelper', 'helper', 'moderator'] }
+    if (userPerms == 'srMod') { perms = ['everyone', 'trialHelper', 'helper', 'moderator', 'srMod'] }
+    if (userPerms == 'admin') { perms = ['everyone', 'trialHelper', 'helper', 'moderator', 'srMod', 'admin'] }
+    if (userPerms == 'owner') { perms = ['everyone', 'trialHelper', 'helper', 'moderator', 'srMod', 'admin', 'owner'] }
 
     return perms
 }
 
 async function checkUserHasPermsForCommand(commandPerms: string, userPerms: string) {
-    let perms = getAllUserPerms(userPerms)
-
-    return perms.includes(commandPerms)
+    return getAllUserPerms(userPerms).includes(commandPerms)
 }
 
 async function checkUserCanUseCommandInChannel(guildID: string, channelID: string, userPerms: string, commandID: string) {
-    let canUse = false
     let channelPerms = false
+    let lockedChannelFound = false
     database.read(guildID).then(database => {
         const db = database[0]
         const lockedChannels = db.guildSettings.lockedChannels
 
         getAllUserPerms(userPerms).forEach(perm => {
-            if (lockedChannels[perm] == channelID) {channelPerms = true}
+            if (lockedChannels[perm] == channelID) {
+                channelPerms = true
+                lockedChannelFound = true
+            }
         })
 
-        console.log(channelPerms)
+        if (lockedChannelFound == false) {channelPerms = true}
+        utils.debug(`channelPerms: ${channelPerms}`)
+        utils.debug(`lockedChannel: ${lockedChannelFound}`)
+        console.log(`\n`)
+        return channelPerms
     })
 }
 
