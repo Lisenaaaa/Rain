1a29db405425aac86c465463abfcf21a9e6c203c
Lisena <lisenawastaken@gmail.com>
2021-10-15 15:09:08 -0700

    add comment to loading.js and confirm grammar there

--- loading.js
+++ loading.js
@@ -1,1 +1,5 @@
-console.log('Transpiling (or whatever it\'s called) TypeScript! This will take a bit of time, so be patient.')
+/*
+This runs before TypeScript transpiles.
+*/
+
+console.log('Transpiling TypeScript! This will take a bit of time, so be patient.')
