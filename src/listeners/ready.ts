84b55704eb96ba17acc412e85ad3370e0d8d1562
Katherine <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-09 09:37:44 -0800

    make the listener be ts instead of js

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,12 +1,12 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Listener, ListenerOptions } from '@sapphire/framework';
+import { Listener, ListenerOptions, SapphireClient } from '@sapphire/framework';
 
 @ApplyOptions<ListenerOptions>({
   once: true,
   event: 'ready'
 })
 export class ReadyListener extends Listener {
-  public run(client: Client) {
-    console.log(`logged in as ${client.user.tag}`)
+  public run(client: SapphireClient) {
+    console.log(`logged in as ${client.user?.tag}`)
   }
 }
