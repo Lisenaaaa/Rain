2c3d5d7f371162ea3120c6afd8d697c710948151
Katherine <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-12 15:27:39 -0800

    this will result in 12 errors

--- src/preconditions/ownerOnly.ts
+++ src/preconditions/ownerOnly.ts
@@ -1,10 +1,9 @@
 import { Precondition } from '@sapphire/framework'
 import { Message } from 'discord.js'
-import users from '../functions/objectfunctions/users'
 
 export class OwnerOnlyPrecondition extends Precondition {
 	public async run(message: Message) {
-		return users.isOwner(message.author)
+		return this.container.users.isOwner(message.author)
 			? this.ok()
 			: this.error({
 					identifier: 'ownerOnly',
