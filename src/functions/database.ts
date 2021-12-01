1291474975ab90852a2464c6c596152cc9eb548e
Lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-30 18:15:59 -0800

    make some literally completely useless changes in how the database functions work

--- src/functions/database.ts
+++ src/functions/database.ts
@@ -65,261 +65,267 @@ export default class Database {
 		}
 	}
 
-	guilds = {
-		async fetchOne(guildID: Snowflake) {
-			const guilds = await this.fetchAll()
-			const guildDB = guilds.find((g: GuildDatabase) => g.guildID == guildID)
+	guilds = new DatabaseGuilds()
+	users = new DatabaseUsers()
+	commands = new DatabaseCommands()
+}
 
-			return guildDB
-		},
+class DatabaseGuilds {
+	public async fetchOne(guildID: Snowflake) {
+		const guilds = await this.fetchAll()
+		const guildDB = guilds.find((g: GuildDatabase) => g.guildID == guildID)
 
-		async fetchAll() {
-			try {
-				const guilddb = await rawDbRequest('SELECT * from guilds;')
-				const alldbs: GuildDatabase[] = []
-
-				/* typescript is stupid and i want eslint to not be yell */
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				guilddb[0].forEach((db: any) => {
-					alldbs.push(db.data)
-				})
-
-				return alldbs
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to fetch guilds.' },
-				})
-				return []
-			}
-		},
-
-		async edit(guildID: Snowflake, query: string, newValue: unknown) {
-			if (query === 'guildID') return false
-			try {
-				const guildDB = (await this.fetchAll()).find(
-					(d: GuildDatabase) => d.guildID == guildID
-				) as GuildDatabase
-				const queryArray = query.split('.')
-				let dbObject: GuildDatabase = guildDB
-				const finalQuery = queryArray.pop()
-
-				queryArray.forEach((query) => {
-					//@ts-ignore ok typescript
-					dbObject = dbObject?.[query as keyof typeof dbObject]
-				})
-
-				//@ts-ignore ok typescript
-				dbObject[finalQuery] = newValue
-
-				await rawDbRequest("UPDATE guilds SET data = $1 WHERE data->>'guildID' = $2;", {
-					bind: [guildDB, guildID],
-				})
-
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to edit a guild.' },
-				})
-				return false
-			}
-		},
-
-		async add(guildID: Snowflake) {
-			if (!guildID) throw new Error('I cannot create a guild database entry without an ID')
-			try {
-				const schema = JSON.stringify(defaultDBSchema(guildID))
-				const string = `INSERT INTO guilds(data) VALUES ($schema);`
-				await rawDbRequest(string, { bind: { schema: schema } })
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to create a guild.' },
-				})
-				return false
-			}
-		},
-
-		async delete(guildID: Snowflake) {
-			try {
-				await rawDbRequest("DELETE FROM guilds WHERE data->>'guildID' = $1;", {
-					bind: [guildID],
-				})
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to delete a guild.' },
-				})
-				return false
-			}
-		},
+		return guildDB
 	}
-	users = {
-		async add(userID: Snowflake) {
-			if (!userID) throw new Error('I cannot create a user database entry without an ID')
-			try {
-				const schema = JSON.stringify(
-					new UserDatabaseConstructor({
-						userID: userID,
-						badges: [],
-						superuser: false,
-						blacklisted: false,
-					})
-				)
-				const string = `INSERT INTO users(data) VALUES ($schema);`
-				await rawDbRequest(string, { bind: { schema: schema } })
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to create a user.' },
-				})
-				return false
-			}
-		},
 
-		async fetchAll() {
-			try {
-				const userdb = await rawDbRequest('SELECT * from users;')
-				const alldbs: { userID: Snowflake; badges: string[]; superuser: boolean }[] = []
+	public async fetchAll() {
+		try {
+			const guilddb = await rawDbRequest('SELECT * from guilds;')
+			const alldbs: GuildDatabase[] = []
 
-				/* typescript is stupid and i want eslint to not be yell */
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				userdb[0].forEach((db: any) => {
-					alldbs.push(db.data)
-				})
+			/* typescript is stupid and i want eslint to not be yell */
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			guilddb[0].forEach((db: any) => {
+				alldbs.push(db.data)
+			})
 
-				return alldbs
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to fetch users.' },
-				})
-				return []
-			}
-		},
-
-		async fetchOne(userID: Snowflake) {
-			const usersDB = await this.fetchAll()
-			return usersDB.find((user) => user.userID === userID)
-		},
-
-		async edit(userID: Snowflake, query: 'badges' | 'superuser', newValue: unknown) {
-			try {
-				const userDB = (await this.fetchOne(userID)) as {
-					userID: Snowflake
-					badges: string[]
-					superuser: boolean
-				}
-
-				const queryArray = query.split('.')
-
-				let dbObject: { userID: Snowflake; badges: string[]; superuser: boolean } = userDB
-
-				const finalQuery = queryArray.pop()
-
-				queryArray.forEach((query) => {
-					//@ts-ignore ok typescript
-					dbObject = dbObject?.[query as keyof typeof dbObject]
-				})
-
-				//@ts-ignore ok typescript
-				dbObject[finalQuery as keyof typeof dbObject] = newValue
-
-				await rawDbRequest("UPDATE users SET data = $1 WHERE data->>'userID' = $2;", {
-					bind: [userDB, userID],
-				})
-
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'Database',
-					data: { note: 'Failed to edit a user.' },
-				})
-				return false
-			}
-		},
+			return alldbs
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to fetch guilds.' },
+			})
+			return []
+		}
 	}
-	commands = {
-		async add(commandID: Snowflake) {
-			if (!commandID)
-				throw new Error('I cannot create a command database entry without an ID')
-			try {
-				const schema = JSON.stringify({ commandID: commandID, enabled: true })
-				const string = `INSERT INTO commands(data) VALUES ($schema);`
-				await rawDbRequest(string, { bind: { schema: schema } })
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to create a command.' },
-				})
-				return false
-			}
-		},
 
-		async fetchAll() {
-			try {
-				const commandsdb = await rawDbRequest('SELECT * from commands;')
-				const alldbs: { commandID: string; enabled: boolean }[] = []
+	public async edit(guildID: Snowflake, query: string, newValue: unknown) {
+		if (query === 'guildID') return false
+		try {
+			const guildDB = (await this.fetchAll()).find(
+				(d: GuildDatabase) => d.guildID == guildID
+			) as GuildDatabase
+			const queryArray = query.split('.')
+			let dbObject: GuildDatabase = guildDB
+			const finalQuery = queryArray.pop()
 
-				/* typescript is stupid and i want eslint to not be yell */
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				commandsdb[0].forEach((db: any) => {
-					alldbs.push(db.data)
-				})
-
-				return alldbs
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to fetch commands.' },
-				})
-				return []
-			}
-		},
-
-		async fetchOne(commandID: string) {
-			const commandsDB = await this.fetchAll()
-			return commandsDB.find((cmd) => cmd.commandID === commandID)
-		},
-
-		async edit(commandID: string, query: 'enabled', newValue: unknown) {
-			try {
-				const commandsDB = (await this.fetchOne(commandID)) as {
-					commandID: string
-					enabled: true
-				}
-
-				const queryArray = query.split('.')
-
-				let dbObject: { commandID: string; enabled: true } = commandsDB
-
-				const finalQuery = queryArray.pop()
-
-				queryArray.forEach((query) => {
-					//@ts-ignore ok typescript
-					dbObject = dbObject?.[query as keyof typeof dbObject]
-				})
-
+			queryArray.forEach((query) => {
 				//@ts-ignore ok typescript
-				dbObject[finalQuery as keyof typeof dbObject] = newValue
+				dbObject = dbObject?.[query as keyof typeof dbObject]
+			})
 
-				await rawDbRequest("UPDATE commands SET data = $1 WHERE data->>'commandID' = $2;", {
-					bind: [commandsDB, commandID],
-				})
+			//@ts-ignore ok typescript
+			dbObject[finalQuery] = newValue
 
-				return true
-			} catch (err) {
-				await container.utils.error(err, {
-					type: 'database',
-					data: { note: 'Failed to edit a command.' },
+			await rawDbRequest("UPDATE guilds SET data = $1 WHERE data->>'guildID' = $2;", {
+				bind: [guildDB, guildID],
+			})
+
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to edit a guild.' },
+			})
+			return false
+		}
+	}
+
+	public async add(guildID: Snowflake) {
+		if (!guildID) throw new Error('I cannot create a guild database entry without an ID')
+		try {
+			const schema = JSON.stringify(defaultDBSchema(guildID))
+			const string = `INSERT INTO guilds(data) VALUES ($schema);`
+			await rawDbRequest(string, { bind: { schema: schema } })
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to create a guild.' },
+			})
+			return false
+		}
+	}
+
+	public async delete(guildID: Snowflake) {
+		try {
+			await rawDbRequest("DELETE FROM guilds WHERE data->>'guildID' = $1;", {
+				bind: [guildID],
+			})
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to delete a guild.' },
+			})
+			return false
+		}
+	}
+}
+
+class DatabaseUsers {
+	public async add(userID: Snowflake) {
+		if (!userID) throw new Error('I cannot create a user database entry without an ID')
+		try {
+			const schema = JSON.stringify(
+				new UserDatabaseConstructor({
+					userID: userID,
+					badges: [],
+					superuser: false,
+					blacklisted: false,
 				})
-				return false
+			)
+			const string = `INSERT INTO users(data) VALUES ($schema);`
+			await rawDbRequest(string, { bind: { schema: schema } })
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to create a user.' },
+			})
+			return false
+		}
+	}
+
+	public async fetchAll() {
+		try {
+			const userdb = await rawDbRequest('SELECT * from users;')
+			const alldbs: { userID: Snowflake; badges: string[]; superuser: boolean }[] = []
+
+			/* typescript is stupid and i want eslint to not be yell */
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			userdb[0].forEach((db: any) => {
+				alldbs.push(db.data)
+			})
+
+			return alldbs
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to fetch users.' },
+			})
+			return []
+		}
+	}
+
+	public async fetchOne(userID: Snowflake) {
+		const usersDB = await this.fetchAll()
+		return usersDB.find((user) => user.userID === userID)
+	}
+
+	public async edit(userID: Snowflake, query: 'badges' | 'superuser', newValue: unknown) {
+		try {
+			const userDB = (await this.fetchOne(userID)) as {
+				userID: Snowflake
+				badges: string[]
+				superuser: boolean
 			}
-		},
+
+			const queryArray = query.split('.')
+
+			let dbObject: { userID: Snowflake; badges: string[]; superuser: boolean } = userDB
+
+			const finalQuery = queryArray.pop()
+
+			queryArray.forEach((query) => {
+				//@ts-ignore ok typescript
+				dbObject = dbObject?.[query as keyof typeof dbObject]
+			})
+
+			//@ts-ignore ok typescript
+			dbObject[finalQuery as keyof typeof dbObject] = newValue
+
+			await rawDbRequest("UPDATE users SET data = $1 WHERE data->>'userID' = $2;", {
+				bind: [userDB, userID],
+			})
+
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'Database',
+				data: { note: 'Failed to edit a user.' },
+			})
+			return false
+		}
+	}
+}
+
+class DatabaseCommands {
+	public async add(commandID: Snowflake) {
+		if (!commandID)
+			throw new Error('I cannot create a command database entry without an ID')
+		try {
+			const schema = JSON.stringify({ commandID: commandID, enabled: true })
+			const string = `INSERT INTO commands(data) VALUES ($schema);`
+			await rawDbRequest(string, { bind: { schema: schema } })
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to create a command.' },
+			})
+			return false
+		}
+	}
+
+	public async fetchAll() {
+		try {
+			const commandsdb = await rawDbRequest('SELECT * from commands;')
+			const alldbs: { commandID: string; enabled: boolean }[] = []
+
+			/* typescript is stupid and i want eslint to not be yell */
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			commandsdb[0].forEach((db: any) => {
+				alldbs.push(db.data)
+			})
+
+			return alldbs
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to fetch commands.' },
+			})
+			return []
+		}
+	}
+
+	public async fetchOne(commandID: string) {
+		const commandsDB = await this.fetchAll()
+		return commandsDB.find((cmd) => cmd.commandID === commandID)
+	}
+
+	public async edit(commandID: string, query: 'enabled', newValue: unknown) {
+		try {
+			const commandsDB = (await this.fetchOne(commandID)) as {
+				commandID: string
+				enabled: true
+			}
+
+			const queryArray = query.split('.')
+
+			let dbObject: { commandID: string; enabled: true } = commandsDB
+
+			const finalQuery = queryArray.pop()
+
+			queryArray.forEach((query) => {
+				//@ts-ignore ok typescript
+				dbObject = dbObject?.[query as keyof typeof dbObject]
+			})
+
+			//@ts-ignore ok typescript
+			dbObject[finalQuery as keyof typeof dbObject] = newValue
+
+			await rawDbRequest("UPDATE commands SET data = $1 WHERE data->>'commandID' = $2;", {
+				bind: [commandsDB, commandID],
+			})
+
+			return true
+		} catch (err) {
+			await container.utils.error(err, {
+				type: 'database',
+				data: { note: 'Failed to edit a command.' },
+			})
+			return false
+		}
 	}
 }
