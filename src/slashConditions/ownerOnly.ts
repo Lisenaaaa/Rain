27a739a0cfff8231bdfea7a18e439a5466e10d46
Lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2021-12-07 13:28:31 -0800

    lots of shit, idk what

---
+++ src/slashConditions/ownerOnly.ts
@@ -0,0 +1,22 @@
+import { PieceContext } from '@sapphire/pieces'
+import { BaseCommandInteraction } from 'discord.js'
+import { SlashPrecondition } from '../structures/SlashCondition'
+
+export class OwnerOnlyCondition extends SlashPrecondition {
+	constructor(context: PieceContext) {
+		super(context, {
+			name: 'ownerOnly'
+		})
+	}
+	public async run(interaction: BaseCommandInteraction) {
+		return this.container.users.isOwner(interaction.user)
+			? this.ok()
+			: this.notOk('This command can only be used by my developers.')
+	}
+}
+
+declare module '@sapphire/framework' {
+	interface Preconditions {
+		slash_ownerOnly: never
+	}
+}
