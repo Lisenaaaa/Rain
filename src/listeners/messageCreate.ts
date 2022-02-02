9514f8fb6d9590a504306e5e9df6ad93fde4b6d2
lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2022-02-02 15:07:36 -0800

    discord is remarkably stupid

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -17,11 +17,15 @@ export class MemberAddListener extends Listener {
 	}
 
 	async yellAtDiscord() {
-		if (this.message.system) {
+		if (this.message.author.discriminator === '0000' && this.message.channel.type != 'DM') {
 			if (this.container.utils.random(10) != 10) return
 
-			// message from KAI#1028 on https://optifine.net/discord - https://canary.discord.com/channels/423430686880301056/426005631997181963/938564033030815874
-			await this.message.reply('I AQHTEW YOU IU HATE YOU DISCOR D YOU SUCK EW I HATE OYU YOU SUCK DISCORD')
+			try {
+				// message from KAI#1028 on https://optifine.net/discord - https://canary.discord.com/channels/423430686880301056/426005631997181963/938564033030815874
+				await this.message.reply('I AQHTEW YOU IU HATE YOU DISCOR D YOU SUCK EW I HATE OYU YOU SUCK DISCORD')
+			} catch (err) {
+				return
+			}
 		}
 	}
 }
