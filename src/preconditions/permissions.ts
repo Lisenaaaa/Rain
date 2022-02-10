9dfd5621608e3553d7358d02d305bc3edb0dd886
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-02-10 15:35:35 -0700

    format

--- src/preconditions/permissions.ts
+++ src/preconditions/permissions.ts
@@ -66,7 +66,7 @@ export class PermissionsPrecondition extends Precondition {
 			label: 'Do I have permissions to run this command?',
 			botPerms: '[potentially massive array]',
 			commandPerms: command.sapphire?.options.botPerms,
-			value: botPerms.every(e => (command.sapphire?.options.botPerms ?? []).includes(e)),
+			value: botPerms.every((e) => (command.sapphire?.options.botPerms ?? []).includes(e)),
 		}
 
 		this.container.logger.debug('making userHasDiscordPerms object')
@@ -75,7 +75,10 @@ export class PermissionsPrecondition extends Precondition {
 			guildHasStaffRoles: await container.guilds.hasStaffRoles(guild),
 			requiredPerms: command.sapphire?.userDiscordPerms ?? [],
 			userPerms: '[potentially massive array]',
-			value: channel.permissionsFor(member).toArray().every(e => (command.sapphire?.userDiscordPerms ?? []).includes(e)),
+			value: channel
+				.permissionsFor(member)
+				.toArray()
+				.every((e) => (command.sapphire?.userDiscordPerms ?? []).includes(e)),
 		}
 
 		this.container.logger.debug('logging all of those objects')
