da3d0204935a3b7287b6eb9b6f5338d354032e49
lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2022-02-20 16:34:07 -0800

    getObjectDifferences function

--- src/functions/utilities.ts
+++ src/functions/utilities.ts
@@ -373,4 +373,17 @@ export default class Utilities {
 		string = newString.join('')
 		return string
 	}
+
+	getObjectDifferences(object1: Record<string, unknown>, object2: Record<string, unknown>) {
+		const finalObject: Record<string, unknown> = {}
+		for (const key of Object.keys(object1)) {
+			if (Object.keys(object2).includes(key)) {
+				if (object1[key] != object2[key]) {
+					finalObject[key] = [object1[key], object2[key]]
+				}
+			}
+		}
+
+		return finalObject
+	}
 }
